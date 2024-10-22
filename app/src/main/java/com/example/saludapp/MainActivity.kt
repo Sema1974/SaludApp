e0eb65105b758f8f4bf85ea2e51d67db4f8c858c
josvidsan <vidalsanchezjm@gamil.com>
2024-10-22 13:57:18 +0200

    Commit acaba la clase dos

--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.saludapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val intentGA = Intent(this, GreetingActivity::class.java)
+        intentGA.putExtra("EXTRA_NAME", name)
+        startActivity(intentGA)
+        val btnHello:Button = findViewById(R.id.btnHello)
+            btnHello.setOnClickListener() {
+                /*val etName:EditText = findViewById(R.id.etName)
+                val name = etName.text.toString()
+                Toast.makeText(this, "Hello $name!", Toast.LENGTH_LONG).show()
+                Log.i("Test", "Boton pulsado" )
+                if (name.isNotEmpty()){
+                    Snackbar.make(btnHello, "Hello $name!", Snackbar.LENGTH_SHORT).setAnchorView(btnHello).setAction("CLOSE"){finishAffinity()}.show()
+                }*/
+
+            }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
