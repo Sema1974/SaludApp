ffff0e12cd8d7bc6bd1950fd10fce7229a337ac9
josvidsan <vidalsanchezjm@gamil.com>
2024-10-23 09:45:53 +0200

    Commit acaba la clase tres

--- app/src/main/java/com/example/saludapp/GreetingActivity.kt
+++ app/src/main/java/com/example/saludapp/GreetingActivity.kt
@@ -2,14 +2,21 @@ package com.example.saludapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class GreetingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_greeting)
+        val tvGreeting:TextView = findViewById(R.id.tvGreeting)
+        val name:String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvGreeting.text="Hola $name"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -8,28 +8,30 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val etName:EditText = findViewById(R.id.etName)
+        val name = etName.text.toString()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val intentGA = Intent(this, GreetingActivity::class.java)
-        intentGA.putExtra("EXTRA_NAME", name)
-        startActivity(intentGA)
-        val btnHello:Button = findViewById(R.id.btnHello)
-            btnHello.setOnClickListener() {
-                /*val etName:EditText = findViewById(R.id.etName)
-                val name = etName.text.toString()
-                Toast.makeText(this, "Hello $name!", Toast.LENGTH_LONG).show()
+        val btnHello:AppCompatButton= findViewById(R.id.btnHello)
+            btnHello.setOnClickListener()
+            {
+                /*Toast.makeText(this, "Hello $name!", Toast.LENGTH_LONG).show()
                 Log.i("Test", "Boton pulsado" )
                 if (name.isNotEmpty()){
                     Snackbar.make(btnHello, "Hello $name!", Snackbar.LENGTH_SHORT).setAnchorView(btnHello).setAction("CLOSE"){finishAffinity()}.show()
                 }*/
-
+                val intentGA = Intent(this, GreetingActivity::class.java)
+                intentGA.putExtra("EXTRA_NAME", name)
+                startActivity(intentGA)
             }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
